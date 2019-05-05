a8b0acb2b3aac0613f9383cc6e80883448843a78
austinjblake <46485261+austinjblake@users.noreply.github.com>
2019-05-05 13:57:56 -0500

    styling modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import headerStyles from './header.module.scss'
 
 const Header = () => {
   return(
-    <header>
-      <h3>Austin J Blake</h3>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to='/'>
+          Austin J Blake
+        </Link>
+      </h1>
       <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About Me</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
-          <li><Link to='/blog'>Blog</Link></li>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About Me</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>Contact</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>Blog</Link>
+          </li>
         </ul>
       </nav>
     </header>
