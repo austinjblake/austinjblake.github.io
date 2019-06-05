68ceaea9fc10ae7fcafa701951ffc9293caf4b0c
austinjblake <46485261+austinjblake@users.noreply.github.com>
2019-06-05 05:57:56 -0500

    adding button links to project template page

--- src/templates/projects.js
+++ src/templates/projects.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Layout from '../components/layout'
 import Head from '../components/head'
+import carouselStyles from '../components/carousel.module.scss'
 
 export const query = graphql`
   query($slug: String!) {
     contentfulProjects(slug: {eq: $slug}){
       name
       description
+      url
+      github
       writeup {
         json
       }
@@ -31,7 +34,16 @@ const Project = (props) => {
       <Head title={props.data.contentfulProjects.name} />
       <h1>{props.data.contentfulProjects.name}</h1>
       <h5>{props.data.contentfulProjects.description}</h5>
+      <div className={carouselStyles.buttonbox}>
+        <a href={props.data.contentfulProjects.url} target="_blank" rel="noopener noreferrer"><button>Live Site</button></a> 
+        <a href={props.data.contentfulProjects.github} target="_blank" rel="noopener noreferrer"><button>Github Repo</button></a> 
+      </div>
       {documentToReactComponents(props.data.contentfulProjects.writeup.json, options)}
+      <div className={carouselStyles.listbutton}>
+        <Link to='/projects'>
+          <button>Complete Project List</button>
+        </Link>
+      </div>
     </Layout>
   )
 }
