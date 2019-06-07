3beecd6752a033b3cbf5f1902c07de178a4a06a8
austinjblake <46485261+austinjblake@users.noreply.github.com>
2019-06-07 10:49:22 -0500

    added icon-link images to contentful and queried. deleted files from repo

--- src/components/icons.js
+++ src/components/icons.js
@@ -5,14 +5,41 @@ import mailImage from './mail.png'
 import linkedinImage from './linkedin.png'
 import iconsStyles from './icons.module.scss'
 import Resume from '../pages/resume.pdf'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Icons = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    mail: contentfulAsset(id: { eq: "e9fc24fa-edce-5535-875a-45593d22594d" }) {
+      file {
+        url
+      }
+    }
+    github: contentfulAsset(id: { eq: "948d0e03-0d10-5216-add7-29ca09d940d9" }) {
+      file {
+        url
+      }
+    }
+    linkedin: contentfulAsset(id: { eq: "ec0b7cf4-05cd-5b77-9b1e-350e18683975" }) {
+      file {
+        url
+      }
+    }
+    resume: contentfulAsset(id: { eq: "dfb71681-1c12-50dc-87e0-8030b8b90eca" }) {
+      file {
+        url
+      }
+    }
+  }
+  `)
+
   return (
+    <div> 
     <div className={iconsStyles.iconList}>
       <a href='https://github.com/austinjblake' target="_blank" rel="noopener noreferrer">
         <img 
           className={iconsStyles.icon} 
-          src={githubImage} 
+          src={data.github.file.url} 
           alt="github"
           title="Checkout my Github profile"
         />
@@ -20,7 +47,7 @@ const Icons = () => {
       <a href={Resume} target="_blank" rel="noopener noreferrer" type="application/pdf">
       <img 
         className={iconsStyles.icon} 
-        src={pdfImage} 
+        src={data.resume.file.url} 
         alt="resume"
         title="View and download a PDF copy of my resume"
       />
@@ -28,7 +55,7 @@ const Icons = () => {
       <a href="mailto:devjobs@austinjblake.dev">
         <img 
           className={iconsStyles.icon} 
-          src={mailImage} 
+          src={data.mail.file.url} 
           alt="email"
           title="Get in touch"
         />
@@ -36,11 +63,12 @@ const Icons = () => {
       <a href='https://www.linkedin.com/in/austin-blake-277333186' target="_blank" rel="noopener noreferrer">
         <img 
           className={iconsStyles.icon} 
-          src={linkedinImage} 
+          src={data.linkedin.file.url} 
           alt="linkedin"
           title="Visit me on LinkedIn"
         />
       </a>
+    </div>
     </div>
   )
 }
