5821c024099bc2f20286da7f9825e4ad7ab147ce
austinjblake <46485261+austinjblake@users.noreply.github.com>
2019-06-10 21:16:45 -0500

    wording

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ const AboutPage = () => {
     <Layout>
       <Head title="About" />
       <h1>About Me</h1>
-      <p>I am a Self-taught developer with experience building real world applications with the latest frameworks and technologies, with a special focus on React. I back all this up with a strong foundational knowledge of Javascript and am constantly expanding and solidifying what I know. I do all of this because I truly enjoy being a developer, solving problems, and learning new things!</p>
+      <p>I am a Self-taught developer building real world applications with the latest frameworks and technologies, with a special focus on React. I back all this up with a strong foundational knowledge of Javascript and am constantly expanding and solidifying what I know. I do all of this because I truly enjoy being a developer, solving problems, and learning new things!</p>
       <p>Currently open to new opportunities in a Developer role--Front End or FullStack</p>
     </Layout>
   )
